9e68813a542b7435700d970f2b14f783c945d1b7
Ben Manley <benjamintmanley@gmail.com>
2023-01-12 20:15:47 -0600

    add axios function

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,14 @@
+import axios from 'axios'
+
+
 function pokedex(req, res) {
-  res.render('pokedex')
+  axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000')
+  .then(response => {
+    console.log(response.data)
+    res.render('pokedex', {
+      pokemons: response.data.results
+    })
+  })
 }
 
 function weather(req, res) {
